cfc9d3e6fd03d1247f6774cf93447fb8a85c1839
maggieruizdiaz20 <maggieruizdiaz22@gmail.com>
2023-09-26 16:52:54 -0400

    adding ash changes

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <div className="main">
-      <video src={landing} loop autoPlay muted />
+      <video className="videoLanding" src={landing} loop autoPlay muted />
       <div className="content">
         <Link to="/home" className="btnLanding">
           Visit Us
