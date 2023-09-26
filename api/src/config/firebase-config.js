74b181301783c6b31df679b25ee7276f1629cea1
maggieruizdiaz20 <maggieruizdiaz22@gmail.com>
2023-09-26 17:09:45 -0400

    adding conditional to firebase

--- api/src/config/firebase-config.js
+++ api/src/config/firebase-config.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const {FIREBASE_SERVICE_ACCOUNT_KEY} = process.env;
-const admin = require("firebase-admin");
+const serviceAccount = FIREBASE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY)
+  : null;
 
-const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY || {});
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
+if (serviceAccount) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  console.error("FIREBASE_SERVICE_ACCOUNT_KEY is not a valid JSON string.");
+}
